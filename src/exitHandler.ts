c6532e282fb0f9e58ec39d36adb0cf5ab311787c
antonisierakowski <antonisierakowski@gmail.com>
2020-07-21 10:56:31 +0200

    Fix process not exiting correctly

---
+++ src/exitHandler.ts
@@ -0,0 +1,10 @@
+import container from './config/container';
+import Knex from 'knex';
+import { dbModule } from './modules/db/serviceIdentifiers';
+
+export const exitHandler = async (): Promise<void> => {
+  console.log('Received SIGINT, initialising graceful shutdown...');
+  const dbConnection = container.get<Knex>(dbModule.ConnectionInstance);
+  await dbConnection.destroy();
+  process.exit();
+};
--- src/run.ts
+++ src/run.ts
@@ -4,17 +4,19 @@ import container from './config/container';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
+import { exitHandler } from './exitHandler';
 
 const port = process.env.REST_API_PORT || 3000;
 
-const server = new InversifyExpressServer(container);
-server.setConfig((app: Application) => {
-  app.use(bodyParser.json());
-});
+(async () => {
+  const server = new InversifyExpressServer(container);
+  server.setConfig((app: Application) => {
+    app.use(bodyParser.json());
+  });
 
-const app = server.build();
-app.listen(port);
+  const app = server.build();
+  app.listen(port);
 
-process.on('SIGINT', () => {
-  console.log('received SIGINT, initialising graceful shutdown...');
-});
+  process.on('SIGINT', exitHandler);
+
+})();
