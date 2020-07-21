6323141fd2e3b349662d2d7e54113be274c5f0dc
antonisierakowski <antonisierakowski@gmail.com>
2020-07-21 18:28:10 +0200

    EventController test suite, bare spec files for remaining suites

---
+++ src/modules/event/__test__/EventController.spec.ts
@@ -0,0 +1,62 @@
+import { EventServiceInterface } from '../EventServiceInterface';
+import { EventJson } from '../types';
+import { EventController } from '../EventController';
+import { Response, Request } from 'express';
+import { createResponse } from '../../../utils/createResponse';
+
+class EventServiceMock implements EventServiceInterface {
+  createEvent(eventJson: EventJson): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
+class EventServiceRejectMock implements EventServiceInterface {
+  createEvent(eventJson: EventJson): Promise<void> {
+    return Promise.reject(new Error());
+  }
+}
+
+// @ts-ignore
+const reqMock: Request = {
+  body: {
+    test: 'test',
+  },
+};
+
+// @ts-ignore
+const resMock: Response = {
+  send: jest.fn(),
+};
+
+describe('EventController', () => {
+  describe('submitEvent method', () => {
+
+    it('should send response status 200 if nothing throws error on the way', async () => {
+      const controller = new EventController(new EventServiceMock());
+      await controller.submitEvent(reqMock, resMock);
+
+      expect(
+        resMock.send,
+      ).toHaveBeenCalledWith(
+        createResponse(200),
+      );
+      expect(
+        resMock.send,
+      ).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call handleError if eventService throws', async () => {
+      const controller = new EventController(new EventServiceRejectMock());
+      await controller.submitEvent(reqMock, resMock);
+
+      expect(
+        resMock.send,
+      ).toHaveBeenCalledWith(
+        createResponse(500, 'Internal Error'),
+      );
+      expect(
+        resMock.send,
+      ).toHaveBeenCalledTimes(1);
+    });
+  });
+});
